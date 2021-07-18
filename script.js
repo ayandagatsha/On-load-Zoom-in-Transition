c99c99aa9f8c7c5bc2f9b79c5a6e0e0ec5c59e9a
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-07-18 07:23:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    document.body.className += " loaded";
+}
+
+let div = document.createElement("div");   // Create a <button> element
+// btn.innerHTML = "CLICK ME";                   // Insert text
+document.body.appendChild(div);               // Append <button> to <body>
+div.id="container"
+
+
+{/* <div >
+<div id="content">
+  This content div is centered within it's containing div
+  <a href="https://css-tricks.com/transitions-only-after-page-load/">Make sure transitions occur only after the page loads, click me.</a>
+</div>
+</div> */}
+
+let div2 = document.createElement("div");   // Create a <button> element
+ div2.innerHTML = "On load Zoom in Transition";                   // Insert text
+div.appendChild(div2);  
+
+div2.id="content";
